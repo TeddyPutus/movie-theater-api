ba91e5360ae56ff187f74a10e0dfaf27c36a53f3
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2022-11-10 15:03:29 +0000

    completed user routes, added screenshots of postman tests

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,56 @@
 const { Router } = require("express");
-const {User} = require('../models/index.js');
+const {User, Show} = require('../models/index.js');
 const userRouter = Router();
 
+//get all users
+userRouter.get('/', async (req, res) => {
+    try {
+        const userList = await User.findAll();
+        res.json(userList);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
+
+//get one user
+userRouter.get('/:user', async (req, res) => {
+    try {
+        const userList = await User.findOne({where: {id: req.params.user}});
+        res.json(userList);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
+
+//get all shows watched by user
+userRouter.get('/:user/shows', async (req, res) => {
+    try {
+        const user = await User.findOne({where: {id: req.params.user}});
+        if(user){
+            const showList = await user.getShows();
+            res.json(showList);
+        }else{
+            res.sendStatus(404);
+        }      
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
+
+//add show to user
+userRouter.put('/:user/shows/:show', async (req, res) => {
+    try {
+        const user = await User.findOne({where: {id: req.params.user}});
+        if(user){
+            const show = await Show.findOne({where: {id: req.params.show}});
+            user.addShow(show);
+            res.sendStatus(200);
+        }else{
+            res.sendStatus(404);
+        }      
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
 
 module.exports = userRouter;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = require("./routes/user");
 const showRouter = require("./routes/user");
 const app = express();
+const seed = require('./seed')
 
 //very important tp include these!! We need this to read the body in our routers
 app.use(express.urlencoded({ extended: false }));
@@ -10,6 +11,8 @@ app.use(express.json());
 //set up  routers
 app.use('/users', userRouter);
 app.use('/shows', showRouter);
+
+seed();
 
 app.listen(5001);
 
